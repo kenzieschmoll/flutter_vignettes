07263bf447d06dc8a1a1b965eeb40986f684911a
Kenzie Schmoll <kenzieschmoll@google.com>
2021-01-28 11:55:22 -0800

    switch stack widgets

--- vignettes/spending_tracker/lib/spending_graph.dart
+++ vignettes/spending_tracker/lib/spending_graph.dart
@@ -88,7 +88,6 @@ class _SpendingGraphState extends State<SpendingGraph> with SingleTickerProvider
         //width: 320 * ScalingInfo.scaleX,
         height: 160 * ScalingInfo.scaleY,
         child: Stack(
-          overflow: Overflow.visible,
           children: [
             Container(
               width: appSize.width,
